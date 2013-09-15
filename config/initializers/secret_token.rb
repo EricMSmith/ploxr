9aa3778db7836a46bda9e14c9cc61eaf8942668b
Eric Smith <glimbit@gmail.com>
2013-09-15 13:16:03 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ploxr::Application.config.secret_key_base = '185ccfb05ce970c89b66f52bd1a23ba8398ddee0e0a8dbb577606e9425e530c7f9f3340399c4ea2cf3a0ef36d63a9e9a70b5e2289c20ab41454e43e99a50f203'

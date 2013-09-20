cee184e729fd000dadb61a1c8eb272abaae8c6e1
Eric Smith <glimbit@gmail.com>
2013-09-20 08:13:15 -0400

    Send page to comic anchor through page nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,8 @@ class PagesController < ApplicationController
 
 	def feed
 		@title = "PLOX : By Steve Hamaker"
-
 		@pages = Page.order("updated_at desc")
-
 		@updated = @pages.first.updated_at unless @pages.empty?
-
 		respond_to do |format|
 			format.atom { render :layout => false }
 			format.rss { redirect_to feed_path(:format => :atom),

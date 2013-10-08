27c8ef72cc783aa830706611ce800c48e7fa8e5f
Eric M. Smith <glimbit@gmail.com>
2013-10-08 14:06:17 -0400

    Get rid of new tests, switch back to old.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,15 +2,42 @@ require 'spec_helper'
 
 describe Book do
   
-  it "has a valid factory" do
-    FactoryGirl.create(:book).should be_valid
+  before do
+  	@book = FactoryGirl.build(:book)
   end
 
-  it "is invalid without a title" do
-    FactoryGirl.build(:book, title: nil).should_not be_valid
+  subject { @book }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:description) }
+  it { should respond_to(:blurb) }
+  it { should respond_to(:author) }
+  it { should respond_to(:author_about) }
+
+  it { should be_valid }
+
+  describe "when title is not present" do
+  	before { @book.title = nil }
+  	it { should_not be_valid }
   end
 
-  it "is invalid without a description" do
-    FactoryGirl.build(:book, description: nil).should_not be_valid
+  describe "when description is not present" do
+  	before { @book.description = nil }
+  	it { should_not be_valid }
+  end
+
+  describe "when blurb is not present" do
+    before { @book.blurb = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when author is not present" do
+  	before { @book.author = nil }
+  	it { should_not be_valid }
+  end
+
+  describe "when about author is not present" do
+  	before { @book.author_about = nil }
+  	it { should_not be_valid }
   end
 end
